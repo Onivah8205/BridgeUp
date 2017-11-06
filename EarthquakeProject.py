c9145ce1701564a1a351aff908a379bc4e419729
SGunal <33435384+SGunal@users.noreply.github.com>
2017-11-06 18:22:24 -0500

    Update EarthquakeProject.py

--- EarthquakeProject.py
+++ EarthquakeProject.py
@@ -16,8 +16,8 @@ image = mpimg.imread ("mapp.jpg")
 for line in earthquakedata:
     #line.split turns each row into a list of individual strings
     line = line.split(',')
-    lat.append(float(line[1]))
-    longitude.append(float(line[2]))
+    lat.append(float(line[1])) #turns 2nd item in list into a float
+    longitude.append(float(line[2])) #turns 3rd item in list into a float
 
 #create scatter plot
 plt.scatter(longitude, lat)
@@ -25,6 +25,7 @@ plt.scatter(longitude, lat)
 #puts image behind scatter plot; extent defines where the image should be
 plt.imshow(image, extent=[-197,196,87,-63])
 
+#plotting x and y labels
 plt.xlabel('Longitude')
 plt.ylabel('Latitude')
-plt.show()
+plt.show() #creates graph
